0b3467e63c2c69a6e72308bf74b26b828cf2d6d7
乔治白 <3157774884@qq.com>
2025-11-01 18:40:46 +0800

    fix: 配置 CommonJS 模块系统以兼容 Vercel Serverless Functions

--- api/index.ts
+++ api/index.ts
@@ -6,5 +6,8 @@
 // 设置环境变量标识为 Vercel 环境
 process.env.VERCEL = '1'
 
-// 导入 Express 应用（Vercel 会自动编译 TypeScript）
-export { default } from '../service/src/index'
+// 导入并重新导出 Express 应用的 handler
+import handler from '../service/src/index'
+
+export default handler
+

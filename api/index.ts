df82e5c8499518f902f4065155750185d4eeed43
乔治白 <3157774884@qq.com>
2025-11-01 19:05:05 +0800

    fix: 构建 service 并导入构建后的文件

--- api/index.ts
+++ api/index.ts
@@ -6,8 +6,8 @@
 // 设置环境变量标识为 Vercel 环境
 process.env.VERCEL = '1'
 
-// 导入并重新导出 Express 应用的 handler
-import handler from '../service/src/index'
+// 导入构建后的 Express 应用
+import handler from '../service/build/index.mjs'
 
 export default handler
 
